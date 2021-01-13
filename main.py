66bf26dd8e69fc5669565ff8a9cbee5cf429b01b
VladimirMashoshin <vladim.mashoshin@yandex.ru>
2021-01-13 16:29:25 +0300

    Lose leads to Main menu

--- main.py
+++ main.py
@@ -11,46 +11,51 @@ if __name__ == '__main__':
     screen = pygame.display.set_mode(size)
     clock = pygame.time.Clock()
     fps = 60
-    running = True
-    game_started = False
-    menu = main_menu(fps, screen)
-    while not game_started and running:
-        pygame.display.update()
-        for event in pygame.event.get():
-            if event.type == pygame.QUIT:
-                running = False
-            if event.type == pygame.KEYDOWN:
-                if event.key == pygame.K_UP:
-                    menu.set_current_chose((menu.get_current_chose() - 1) % 2)
-                elif event.key == pygame.K_DOWN:
-                    menu.set_current_chose((menu.get_current_chose() + 1) % 2)
-                elif event.key == pygame.K_SPACE:
-                    if menu.get_current_chose() == 0:
+    exit_confirmed = False
+    while not exit_confirmed:
+        running = True
+        game_started = False
+        menu = main_menu(fps, screen)
+        while not game_started and running:
+            pygame.display.update()
+            for event in pygame.event.get():
+                if event.type == pygame.QUIT:
+                    running = False
+                    exit_confirmed = True
+                if event.type == pygame.KEYDOWN:
+                    if event.key == pygame.K_UP:
+                        menu.set_current_chose((menu.get_current_chose() - 1) % 2)
+                    elif event.key == pygame.K_DOWN:
+                        menu.set_current_chose((menu.get_current_chose() + 1) % 2)
+                    elif event.key == pygame.K_SPACE:
+                        if menu.get_current_chose() == 0:
+                            game_started = True
+                        else:
+                            running = False
+                            exit_confirmed = True
+            menu.draw()
+        stack_rating = 0
+        game = Tetris(fps, screen, stack_rating)
+        while running:
+            pygame.display.update()
+            for event in pygame.event.get():
+                if event.type == pygame.QUIT:
+                    running = False
+                    exit_confirmed = True
+                if event.type == pygame.KEYDOWN:
+                    if not game_started:
+                        game = Tetris(fps, screen)
                         game_started = True
-                    else:
-                        running = False
-        menu.draw()
-    stack_rating = 0
-    game = Tetris(fps, screen, stack_rating)
-    while running:
-        pygame.display.update()
-        for event in pygame.event.get():
-            if event.type == pygame.QUIT:
+                    game.on_key_pressed(event.key)
+            screen.fill(pygame.Color('black'))
+            game.render(screen)
+            game.update()
+            pygame.display.flip()
+            clock.tick(fps)
+            if not game.running:
                 running = False
-            if event.type == pygame.KEYDOWN:
-                if not game_started:
-                    game = Tetris(fps, screen)
-                    game_started = True
-                game.on_key_pressed(event.key)
-        screen.fill(pygame.Color('black'))
-        game.render(screen)
-        game.update()
-        pygame.display.flip()
-        clock.tick(fps)
-        if not game.running:
-            running = False
-            while not game.confirmed_lose:
-                for event in pygame.event.get():
-                    if event.type == pygame.QUIT or event.type == pygame.KEYDOWN:
-                        game.confirm_lose()
+                while not game.confirmed_lose:
+                    for event in pygame.event.get():
+                        if event.type == pygame.QUIT or event.type == pygame.KEYDOWN:
+                            game.confirm_lose()
     pygame.quit()
